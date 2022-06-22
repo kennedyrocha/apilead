9d997eab4fed8500ec3085afc5b959ca85ea604e
kennedyrocha <kennedy279972@gmail.com>
2022-06-22 15:26:31 -0400

    public insert on lead

--- src/main/java/br/com/olx/leadIntegration/config/SecurityConfig.java
+++ src/main/java/br/com/olx/leadIntegration/config/SecurityConfig.java
@@ -16,6 +16,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	private static final String[] PUBLIC_MATCHERS_GET = {
 			"/"
 	};
+
+	
+	private static final String[] PUBLIC_MATCHERS_POST = {
+			"/leads"
+	};
 	
     @Override
     protected void configure(HttpSecurity http) throws Exception
@@ -24,11 +29,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
 			    .authorizeRequests()
 			    .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			    .antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
                 .anyRequest().authenticated()
                 .and().httpBasic()
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
-
 }
-
-
